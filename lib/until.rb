984ead84088f3fc5fda9d754af84d805c0a9535f
ozPop <ozziepopovas@gmail.com>
2016-05-04 19:23:49 +0000

    nitrous-backup at Wed May  4 19:23:49 UTC 2016

--- lib/until.rb
+++ lib/until.rb
@@ -2,12 +2,9 @@ def using_until
   levitation_force = 6
   #your code here
   phrase = "Wingardium Leviosa"
-  loop do
+  until levitation_force == 10
     puts phrase
     levitation_force += 1
-    if levitation_force == 10
-      break
-    end
   end
 end
 
